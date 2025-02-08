33165e65b53f3610e92a6ade9c0cd4b12a9d4f5a
= <niccolo.dellarocca@gmail.com>
2025-02-08 13:28:50 +0100

    Attempt to fix misnamed offerings tab

--- Sources/ZTronCarouselDataSource/mockups/Treyarch/Black Ops 4/Tag Der Toten/makeTagDerToten.swift
+++ Sources/ZTronCarouselDataSource/mockups/Treyarch/Black Ops 4/Tag Der Toten/makeTagDerToten.swift
@@ -3,7 +3,7 @@ import ZTronSerializable
 func makeTagDerToten() -> SerializableMapNode {
     let tagDerTotenTabs = SerializableTabsRouter()
     
-    tagDerTotenTabs.router.register(makeAOTRTEasterEggTab(), at: [">", "iw.aotrt.ee.tab.name"])
+    tagDerTotenTabs.router.register(makeTDTSideQuestsTab(), at: [">", "bo4.tdt.side.quests.tab.name"])
     
     return SerializableMapNode(
         name: "tag der toten",
