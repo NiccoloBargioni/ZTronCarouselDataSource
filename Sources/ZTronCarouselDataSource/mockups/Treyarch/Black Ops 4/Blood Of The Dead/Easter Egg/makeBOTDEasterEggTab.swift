49288f079f10e24cbefd1d1e494bef911ec29b7f
= <niccolo.dellarocca@gmail.com>
2025-04-12 15:32:19 +0200

    Fixed tabs positions for BO4/BOTD

--- Sources/ZTronCarouselDataSource/mockups/Treyarch/Black Ops 4/Blood Of The Dead/Easter Egg/makeBOTDEasterEggTab.swift
+++ Sources/ZTronCarouselDataSource/mockups/Treyarch/Black Ops 4/Blood Of The Dead/Easter Egg/makeBOTDEasterEggTab.swift
@@ -3,7 +3,7 @@ import ZTronSerializable
 public func makeBOTDEasterEggTab() -> SerializableTabNode {
     return SerializableTabNode(
         name: "easter egg",
-        position: 1,
+        position: 0,
         iconName: "oval.portrait.righthalf.filled",
         tools: makeBOTDEasterEggTools()
     )
