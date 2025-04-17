99672af6a3e2ddedaab26b3a1755341e180f004b
= <niccolo.dellarocca@gmail.com>
2025-04-17 11:51:18 +0200

    Fixed makeSmallStatuesTool

--- Sources/ZTronCarouselDataSource/mockups/Infinity Ward/Infinite Warfare/Rave/Side Quests/Crossbow Small Statues/makeSmallStatuesTool.swift
+++ Sources/ZTronCarouselDataSource/mockups/Infinity Ward/Infinite Warfare/Rave/Side Quests/Crossbow Small Statues/makeSmallStatuesTool.swift
@@ -5,6 +5,6 @@ public func makeSmallStatuesTool() -> SerializableToolNode {
         name: "crossbow small statues",
         position: 0,
         assetsImageName: "iw.ritr.side.quests.crossbow.small.statues.icon",
-        galleryRouter: makeSkullhopStep1()
+        galleryRouter: makeRITRCrossbowSmallStatues()
     )
 }
