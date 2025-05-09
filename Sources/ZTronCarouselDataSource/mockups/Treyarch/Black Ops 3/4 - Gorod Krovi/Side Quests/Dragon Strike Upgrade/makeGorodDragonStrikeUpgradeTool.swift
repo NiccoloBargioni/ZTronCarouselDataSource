6fdb61921a0eb9966ff19e9cb9b601183cd4d13e
= <niccolo.dellarocca@gmail.com>
2025-05-09 16:06:54 +0200

    Fixed BO3/GK/SQ/DRAGON STRIKE UPGRADE

--- Sources/ZTronCarouselDataSource/mockups/Treyarch/Black Ops 3/4 - Gorod Krovi/Side Quests/Dragon Strike Upgrade/makeGorodDragonStrikeUpgradeTool.swift
+++ Sources/ZTronCarouselDataSource/mockups/Treyarch/Black Ops 3/4 - Gorod Krovi/Side Quests/Dragon Strike Upgrade/makeGorodDragonStrikeUpgradeTool.swift
@@ -3,7 +3,7 @@ import ZTronSerializable
 public func makeGorodDragonStrikeUpgradeTool() -> SerializableToolNode {
     return SerializableToolNode(
         name: "bo3.gk.side.quests.dragon.strike.upgrade.tool.name",
-        position: 32,
+        position: 2,
         assetsImageName: "bo3.gk.side.quests.dragon.strike.upgrade.icon",
         galleryRouter: makeGorodDragonStrikeUpgrade()
     )
