1b4afad7f6c988cfc04a117cc6a38c6d5476cbc8
= <niccolo.dellarocca@gmail.com>
2025-05-22 23:26:50 +0200

    Fixed position for BO4/DEAD OF THE NIGHT/SIDE QUESTS

--- Sources/ZTronCarouselDataSource/mockups/Treyarch/Black Ops 4/5 - Dead of the night/Side Quests/Wonder Weapon Letters/makeWonderWeaponLettersTool.swift
+++ Sources/ZTronCarouselDataSource/mockups/Treyarch/Black Ops 4/5 - Dead of the night/Side Quests/Wonder Weapon Letters/makeWonderWeaponLettersTool.swift
@@ -3,7 +3,7 @@ import ZTronSerializable
 public func makeWonderWeaponLettersTool() -> SerializableToolNode {
     return SerializableToolNode(
         name: "bo4.dotn.side.quests.perk.locations.from.doors.tool.name",
-        position: 3,
+        position: 4,
         assetsImageName: "bo4.dotn.side.quests.perk.locations.from.doors.icon",
         galleryRouter: makePerkLocationsFromDoors()
     )
