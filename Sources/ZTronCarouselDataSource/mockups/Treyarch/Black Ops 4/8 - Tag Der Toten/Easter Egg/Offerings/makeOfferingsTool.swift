b9c7461a48d6a0db8c0c151af51001a4f9931c0c
= <niccolo.dellarocca@gmail.com>
2025-05-24 14:07:27 +0200

    Fixed namespacing for BO4/TAG DER TOTEN/OFFERINGS

--- Sources/ZTronCarouselDataSource/mockups/Treyarch/Black Ops 4/8 - Tag Der Toten/Easter Egg/Offerings/makeOfferingsTool.swift
+++ Sources/ZTronCarouselDataSource/mockups/Treyarch/Black Ops 4/8 - Tag Der Toten/Easter Egg/Offerings/makeOfferingsTool.swift
@@ -2,7 +2,7 @@ import ZTronSerializable
 
 public func makeOfferingsTool() -> SerializableToolNode {
     return SerializableToolNode(
-        name: "bo4.tdt.easter.egg.offerings",
+        name: "bo4.tdt.easter.egg.offerings.tool.name",
         position: 5,
         assetsImageName: "bo4.tdt.easter.egg.offerings.icon",
         galleryRouter: makeOfferings()
