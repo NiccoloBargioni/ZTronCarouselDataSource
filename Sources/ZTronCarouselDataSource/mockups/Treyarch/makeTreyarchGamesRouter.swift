2b5ceec3ecfb5c2481588c545a8e264f200c64aa
= <niccolo.dellarocca@gmail.com>
2025-06-22 14:23:20 +0200

    Added Black Ops 6

--- Sources/ZTronCarouselDataSource/mockups/Treyarch/makeTreyarchGamesRouter.swift
+++ Sources/ZTronCarouselDataSource/mockups/Treyarch/makeTreyarchGamesRouter.swift
@@ -3,6 +3,7 @@ import ZTronSerializable
 public func make3ArcGamesRouter() -> SerializableGamesRouter {
     let all3ArcGames = SerializableGamesRouter()
     
+    all3ArcGames.router.register(makeBo4(), at: [">", "black ops 6"])
     all3ArcGames.router.register(makeBo4(), at: [">", "black ops 4"])
     all3ArcGames.router.register(makeBO3(), at: [">", "black ops 3"])
     all3ArcGames.router.register(makeBo2(), at: [">", "black ops 2"])
