373563c6962f26f3060afa06b9510a0f169b2e37
= <niccolo.dellarocca@gmail.com>
2025-06-26 16:36:56 +0200

    Added outlines for reels

--- Sources/ZTronCarouselDataSource/mockups/Infinity Ward/Infinite Warfare/4 - Attack/Easter Egg/Braille Code/makeBrailleCode.swift
+++ Sources/ZTronCarouselDataSource/mockups/Infinity Ward/Infinite Warfare/4 - Attack/Easter Egg/Braille Code/makeBrailleCode.swift
@@ -9,15 +9,39 @@ public func makeBrailleCode() -> SerializableGalleryRouter {
         SerializableImageNode(
             name: "iw.aotrt.ee.braille.code.reel.1",
             description: "iw.aotrt.ee.braille.code.reel.1.caption",
-            position: 0
+            position: 0,
+            overlays: [
+                SerializableBoundingCircleNode(),
+                SerializableOutlineNode(
+                    resourceName: "iw.aotrt.ee.braille.code.reel.1.outline",
+                    boundingBox: .init(
+                        x: 370.0 / 1920.0,
+                        y: 571.0 / 1080.0,
+                        width: 28.0 / 1920.0,
+                        height: 11.0 / 1080.0
+                    )
+                )
+            ]
         ), at: [">", "iw.aotrt.ee.braille.code.reel.1"]
     )
     
     reelLocations.register(
         SerializableImageNode(
             name: "iw.aotrt.ee.braille.code.reel.2",
-            description: "iw.aotrt.ee.braille.code.reel.2",
-            position: 1
+            description: "iw.aotrt.ee.braille.code.reel.2.caption",
+            position: 1,
+            overlays: [
+                SerializableBoundingCircleNode(),
+                SerializableOutlineNode(
+                    resourceName: "iw.aotrt.ee.braille.code.reel.2.outline",
+                    boundingBox: .init(
+                        x: 1791.0 / 1920.0,
+                        y: 456.0 / 1080.0,
+                        width: 53.0 / 1920.0,
+                        height: 42.0 / 1080.0
+                    )
+                )
+            ]
         ), at: [">", "iw.aotrt.ee.braille.code.reel.2"]
     )
     
@@ -26,6 +50,18 @@ public func makeBrailleCode() -> SerializableGalleryRouter {
             name: "iw.aotrt.ee.braille.code.reel.3",
             description: "iw.aotrt.ee.braille.code.reel.3.caption",
             position: 2,
+            overlays: [
+                SerializableBoundingCircleNode(),
+                SerializableOutlineNode(
+                    resourceName: "iw.aotrt.ee.braille.code.reel.3.outline",
+                    boundingBox: .init(
+                        x: 1561.0 / 1920.0,
+                        y: 746.0 / 1080.0,
+                        width: 54.0 / 1920.0,
+                        height: 23.0 / 1080.0
+                    )
+                )
+            ]
         ), at: [">", "iw.aotrt.ee.braille.code.reel.3"]
     )
     
@@ -33,15 +69,39 @@ public func makeBrailleCode() -> SerializableGalleryRouter {
         SerializableImageNode(
             name: "iw.aotrt.ee.braille.code.reel.4",
             description: "iw.aotrt.ee.braille.code.reel.4.caption",
-            position: 3
+            position: 3,
+            overlays: [
+                SerializableBoundingCircleNode(),
+                SerializableOutlineNode(
+                    resourceName: "iw.aotrt.ee.braille.code.reel.4.outline",
+                    boundingBox: .init(
+                        x: 139.0 / 1920.0,
+                        y: 528.0 / 1080.0,
+                        width: 53.0 / 1920.0,
+                        height: 22.0 / 1080.0
+                    )
+                )
+            ]
         ), at: [">", "iw.aotrt.ee.braille.code.reel.4"]
     )
-    
+
     reelLocations.register(
         SerializableImageNode(
             name: "iw.aotrt.ee.braille.code.reel.5",
             description: "iw.aotrt.ee.braille.code.reel.5.caption",
-            position: 4
+            position: 4,
+            overlays: [
+                SerializableBoundingCircleNode(),
+                SerializableOutlineNode(
+                    resourceName: "iw.aotrt.ee.braille.code.reel.5.outline",
+                    boundingBox: .init(
+                        x: 1622.0 / 1920.0,
+                        y: 664.0 / 1080.0,
+                        width: 101.0 / 1920.0,
+                        height: 31.0 / 1080.0
+                    )
+                )
+            ]
         ), at: [">", "iw.aotrt.ee.braille.code.reel.5"]
     )
     
