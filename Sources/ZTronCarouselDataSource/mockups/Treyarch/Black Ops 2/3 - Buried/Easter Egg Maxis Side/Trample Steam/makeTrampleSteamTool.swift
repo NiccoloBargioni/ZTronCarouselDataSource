4f405bc94ee1f22eaee7c28c856e7752d82b50a1
= <niccolo.dellarocca@gmail.com>
2025-06-26 16:51:44 +0200

    Fixed maxis side for Buried

--- Sources/ZTronCarouselDataSource/mockups/Treyarch/Black Ops 2/3 - Buried/Easter Egg Maxis Side/Trample Steam/makeTrampleSteamTool.swift
+++ Sources/ZTronCarouselDataSource/mockups/Treyarch/Black Ops 2/3 - Buried/Easter Egg Maxis Side/Trample Steam/makeTrampleSteamTool.swift
@@ -3,7 +3,7 @@ import ZTronSerializable
 public func makeTrampleSteamTool() -> SerializableToolNode {
     return SerializableToolNode(
         name: "bo2.buried.easter.egg.maxis.trample.steam.tool.name",
-        position: 4,
+        position: 3,
         assetsImageName: "bo2.buried.easter.egg.maxis.trample.steam.icon",
         galleryRouter: makeTrampleSteam()
     )
--- Sources/ZTronCarouselDataSource/mockups/Treyarch/Black Ops 2/3 - Buried/Easter Egg Maxis Side/Turbine/makeBuriedTurbineTool.swift
+++ Sources/ZTronCarouselDataSource/mockups/Treyarch/Black Ops 2/3 - Buried/Easter Egg Maxis Side/Turbine/makeBuriedTurbineTool.swift
@@ -3,7 +3,7 @@ import ZTronSerializable
 public func makeBuriedTurbineTool() -> SerializableToolNode {
     return SerializableToolNode(
         name: "bo2.buried.easter.egg.maxis.turbine.tool.name",
-        position: 5,
+        position: 4,
         assetsImageName: "bo2.buried.easter.egg.maxis.turbine.icon",
         galleryRouter: makeBuriedTurbine()
     )
