13baabeeaad7e41374243b20877589a1543241dc
= <niccolo.dellarocca@gmail.com>
2025-07-11 14:35:46 +0200

    Added outlines for IW/RITR/EE/BOAT, IW/RITR/SQ/PAP

--- Sources/ZTronCarouselDataSource/mockups/Infinity Ward/Infinite Warfare/2 - Rave In The Redwoods/Easter Egg/Boat/makeRITRBoatPieces.swift
+++ Sources/ZTronCarouselDataSource/mockups/Infinity Ward/Infinite Warfare/2 - Rave In The Redwoods/Easter Egg/Boat/makeRITRBoatPieces.swift
@@ -7,7 +7,19 @@ public func makeRITRBoatPieces() -> SerializableGalleryRouter {
         SerializableImageNode(
             name: "iw.ritr.easter.egg.boat.pieces.1.inside.mess.hall",
             description: "iw.ritr.easter.egg.boat.pieces.1.inside.mess.hall.caption",
-            position: 0
+            position: 0,
+            overlays: [
+                SerializableBoundingCircleNode(),
+                SerializableOutlineNode(
+                    resourceName: "iw.ritr.easter.egg.boat.pieces.1.inside.mess.hall.outline",
+                    boundingBox: .init(
+                        x: 1284.0 / 2715.0,
+                        y: 789.0 / 1527.0,
+                        width: 46.0 / 2715.0,
+                        height: 71.0 / 1527.0
+                    )
+                )
+            ]
     ), at: ["iw.ritr.easter.egg.boat.pieces.1.inside.mess.hall"])
 
 
@@ -15,7 +27,19 @@ public func makeRITRBoatPieces() -> SerializableGalleryRouter {
         SerializableImageNode(
             name: "iw.ritr.easter.egg.boat.pieces.2.bear.lodge.jugg.room",
             description: "iw.ritr.easter.egg.boat.pieces.2.bear.lodge.jugg.room.caption",
-            position: 1
+            position: 1,
+            overlays: [
+                SerializableBoundingCircleNode(),
+                SerializableOutlineNode(
+                    resourceName: "iw.ritr.easter.egg.boat.pieces.2.bear.lodge.jugg.room.outline",
+                    boundingBox: .init(
+                        x: 1942.0 / 2715.0,
+                        y: 838.0 / 1527.0,
+                        width: 125.0 / 2715.0,
+                        height: 81.0 / 1527.0
+                    )
+                )
+            ]
     ), at: ["iw.ritr.easter.egg.boat.pieces.2.bear.lodge.jugg.room"])
 
 
@@ -23,10 +47,20 @@ public func makeRITRBoatPieces() -> SerializableGalleryRouter {
         SerializableImageNode(
             name: "iw.ritr.easter.egg.boat.pieces.3.recreational.area.front.of.quickies",
             description: "iw.ritr.easter.egg.boat.pieces.3.recreational.area.front.of.quickies.caption",
-            position: 2
+            position: 2,
+            overlays: [
+                SerializableBoundingCircleNode(),
+                SerializableOutlineNode(
+                    resourceName: "iw.ritr.easter.egg.boat.pieces.3.recreational.area.front.of.quickies.outline",
+                    boundingBox: .init(
+                        x: 1748.0 / 2715.0,
+                        y: 837.0 / 1527.0,
+                        width: 89.0 / 2715.0,
+                        height: 305.0 / 1527.0
+                    )
+                )
+            ]
     ), at: ["iw.ritr.easter.egg.boat.pieces.3.recreational.area.front.of.quickies"])
-
-
 
 
     let boatRouter = SerializableGalleryRouter()
--- Sources/ZTronCarouselDataSource/mockups/Infinity Ward/Infinite Warfare/2 - Rave In The Redwoods/Side Quests/Pack-a-Punch/makeRITRPackAPunch.swift
+++ Sources/ZTronCarouselDataSource/mockups/Infinity Ward/Infinite Warfare/2 - Rave In The Redwoods/Side Quests/Pack-a-Punch/makeRITRPackAPunch.swift
@@ -7,7 +7,19 @@ public func makeRITRPackAPunch() -> SerializableGalleryRouter {
         SerializableImageNode(
             name: "iw.ritr.side.quests.pack.a.punch.1.reel.outside.harbor",
             description: "iw.ritr.side.quests.pack.a.punch.1.reel.outside.harbor.caption",
-            position: 0
+            position: 0,
+            overlays: [
+                SerializableBoundingCircleNode(),
+                SerializableOutlineNode(
+                    resourceName: "iw.ritr.side.quests.pack.a.punch.1.reel.outside.harbor.outline",
+                    boundingBox: .init(
+                        x: 1166.0 / 2715.0,
+                        y: 967.0 / 1527.0,
+                        width: 69.0 / 2715.0,
+                        height: 26.0 / 1527.0
+                    )
+                )
+            ]
     ), at: ["iw.ritr.side.quests.pack.a.punch.1.reel.outside.harbor"])
 
 
@@ -15,7 +27,19 @@ public func makeRITRPackAPunch() -> SerializableGalleryRouter {
         SerializableImageNode(
             name: "iw.ritr.side.quests.pack.a.punch.2.turtle.island.outside.kevin.lighthouse",
             description: "iw.ritr.side.quests.pack.a.punch.2.turtle.island.outside.kevin.lighthouse.caption",
-            position: 1
+            position: 1,
+            overlays: [
+                SerializableBoundingCircleNode(),
+                SerializableOutlineNode(
+                    resourceName: "iw.ritr.side.quests.pack.a.punch.2.turtle.island.outside.kevin.lighthouse.outline",
+                    boundingBox: .init(
+                        x: 948.0 / 2715.0,
+                        y: 1245.0 / 1527.0,
+                        width: 82.0 / 2715.0,
+                        height: 54.0 / 1527.0
+                    )
+                )
+            ]
     ), at: ["iw.ritr.side.quests.pack.a.punch.2.turtle.island.outside.kevin.lighthouse"])
 
 
