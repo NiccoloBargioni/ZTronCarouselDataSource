8aafd16c3ff94711ff57620222eadaead42358b3
= <niccolo.dellarocca@gmail.com>
2025-07-11 13:57:48 +0200

    Adjusted fairies

--- Sources/ZTronCarouselDataSource/mockups/Infinity Ward/Infinite Warfare/2 - Rave In The Redwoods/Side Quests/Fairies/makeFairiesCaves.swift
+++ Sources/ZTronCarouselDataSource/mockups/Infinity Ward/Infinite Warfare/2 - Rave In The Redwoods/Side Quests/Fairies/makeFairiesCaves.swift
@@ -3,44 +3,46 @@ import ZTronSerializable
 public func makeFairiesCaves() -> SerializableGalleryNode {
     let fairiesLocations = MediaRouter.init()
     
+    
     fairiesLocations.register(
         SerializableImageNode(
-            name: "iw.ritr.side.quests.fairies.caves.1.caves.to.recreational.area",
-            description: "iw.ritr.side.quests.fairies.caves.1.caves.to.recreational.area.caption",
+            name: "iw.ritr.side.quests.fairies.caves.1.recreational.area.before.mines",
+            description: "iw.ritr.side.quests.fairies.caves.1.recreational.area.before.mines.caption",
             position: 0
-    ), at: ["iw.ritr.side.quests.fairies.caves.1.caves.to.recreational.area"])
+    ), at: ["iw.ritr.side.quests.fairies.caves.1.recreational.area.before.mines"])
+
 
     
     fairiesLocations.register(
         SerializableImageNode(
-            name: "iw.ritr.side.quests.fairies.caves.2.power.room.entrance",
-            description: "iw.ritr.side.quests.fairies.caves.2.power.room.entrance.caption",
+            name: "iw.ritr.side.quests.fairies.caves.2.caves.to.recreational.area",
+            description: "iw.ritr.side.quests.fairies.caves.2.caves.to.recreational.area.caption",
             position: 1
-    ), at: ["iw.ritr.side.quests.fairies.caves.2.power.room.entrance"])
+    ), at: ["iw.ritr.side.quests.fairies.caves.2.caves.to.recreational.area"])
 
     
     fairiesLocations.register(
         SerializableImageNode(
-            name: "iw.ritr.side.quests.fairies.caves.3.across.bang.bangs",
-            description: "iw.ritr.side.quests.fairies.caves.3.across.bang.bangs.caption",
+            name: "iw.ritr.side.quests.fairies.caves.3.power.room.entrance",
+            description: "iw.ritr.side.quests.fairies.caves.3.power.room.entrance.caption",
             position: 2
-    ), at: ["iw.ritr.side.quests.fairies.caves.3.across.bang.bangs"])
+    ), at: ["iw.ritr.side.quests.fairies.caves.3.power.room.entrance"])
 
     
     fairiesLocations.register(
         SerializableImageNode(
-            name: "iw.ritr.side.quests.fairies.caves.4.bang.bangs",
-            description: "iw.ritr.side.quests.fairies.caves.4.bang.bangs.caption",
+            name: "iw.ritr.side.quests.fairies.caves.4.across.bang.bangs",
+            description: "iw.ritr.side.quests.fairies.caves.4.across.bang.bangs.caption",
             position: 3
-    ), at: ["iw.ritr.side.quests.fairies.caves.4.bang.bangs"])
+    ), at: ["iw.ritr.side.quests.fairies.caves.4.across.bang.bangs"])
 
     
     fairiesLocations.register(
         SerializableImageNode(
-            name: "iw.ritr.side.quests.fairies.caves.5.door.to.cabins",
-            description: "iw.ritr.side.quests.fairies.caves.5.door.to.cabins.caption",
+            name: "iw.ritr.side.quests.fairies.caves.5.bang.bangs",
+            description: "iw.ritr.side.quests.fairies.caves.5.bang.bangs.caption",
             position: 4
-    ), at: ["iw.ritr.side.quests.fairies.caves.5.door.to.cabins"])
+    ), at: ["iw.ritr.side.quests.fairies.caves.5.bang.bangs"])
 
 
     return SerializableGalleryNode(
