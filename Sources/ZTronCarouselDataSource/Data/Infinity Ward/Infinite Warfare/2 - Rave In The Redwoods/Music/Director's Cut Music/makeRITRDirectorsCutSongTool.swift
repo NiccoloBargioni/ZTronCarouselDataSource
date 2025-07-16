3f17d151129095e6b26e0862c3c5d8053f675508
= <niccolo.dellarocca@gmail.com>
2025-07-16 13:34:50 +0200

    Fixed position for IW/RAVE/MUSIC

--- Sources/ZTronCarouselDataSource/Data/Infinity Ward/Infinite Warfare/2 - Rave In The Redwoods/Music/Director's Cut Music/makeRITRDirectorsCutSongTool.swift
+++ Sources/ZTronCarouselDataSource/Data/Infinity Ward/Infinite Warfare/2 - Rave In The Redwoods/Music/Director's Cut Music/makeRITRDirectorsCutSongTool.swift
@@ -3,7 +3,7 @@ import ZTronSerializable
 public func makeRITRDirectorsCutSongTool() -> SerializableToolNode {
     return SerializableToolNode(
         name: "iw.ritr.music.directors.cut.song.tool.name",
-        position: 2,
+        position: 1,
         assetsImageName: "iw.ritr.music.directors.cut.song.icon",
         galleryRouter: makeRITRDirectorsCutSong()
     )
