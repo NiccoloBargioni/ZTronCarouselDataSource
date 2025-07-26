3d04fd6ceb36e05e90e4cdba7e4b64cf42a15392
= <niccolo.dellarocca@gmail.com>
2025-07-26 17:14:57 +0200

    Added The Tomb map reference in BO6

--- Sources/ZTronCarouselDataSource/Data/Treyarch/Black Ops 6/makeBO6Maps.swift
+++ Sources/ZTronCarouselDataSource/Data/Treyarch/Black Ops 6/makeBO6Maps.swift
@@ -5,6 +5,7 @@ public func makeBO6Maps() -> SerializableMapsRouter {
     
     bo6Maps.router.register(makeTerminus(), at: [">", "terminus"])
     bo6Maps.router.register(makeCitadelleDesMorts(), at: [">", "cotadelle des morts"])
+    bo6Maps.router.register(makeTheTomb(), at: [">", "the tomb"])
     bo6Maps.router.register(makeShatteredVeil(), at: [">", "shattered veil"])
 
     return bo6Maps
