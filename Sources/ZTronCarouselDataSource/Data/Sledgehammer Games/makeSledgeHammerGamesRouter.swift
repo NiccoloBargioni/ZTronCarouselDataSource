6900b71eb4a5785fd8517c0811633779996bd001
= <niccolo.dellarocca@gmail.com>
2025-09-13 14:27:12 +0200

    Plugged in COD Vanguard

--- Sources/ZTronCarouselDataSource/Data/Sledgehammer Games/makeSledgeHammerGamesRouter.swift
+++ Sources/ZTronCarouselDataSource/Data/Sledgehammer Games/makeSledgeHammerGamesRouter.swift
@@ -3,6 +3,7 @@ import ZTronSerializable
 public func makeSledgeHammerGamesRouter() -> SerializableGamesRouter {
     let allSledgeHammerGames = SerializableGamesRouter()
     
+    allSledgeHammerGames.router.register(makeCODVanguard(), at: [">", "cod vanguard"])
     allSledgeHammerGames.router.register(makeWWII(), at: [">", "wwii"])
     allSledgeHammerGames.router.register(makeAdvancedWarfare(), at: [">", "aw"])
     
