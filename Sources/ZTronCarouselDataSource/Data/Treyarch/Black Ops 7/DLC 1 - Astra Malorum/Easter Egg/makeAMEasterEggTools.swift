65c5911fb1f5bfb2e5b8814f2876be46f51d168f
= <niccolo.dellarocca@gmail.com>
2026-03-16 16:42:13 +0100

    Added stubs for three solvers

--- Sources/ZTronCarouselDataSource/Data/Treyarch/Black Ops 7/DLC 1 - Astra Malorum/Easter Egg/makeAMEasterEggTools.swift
+++ Sources/ZTronCarouselDataSource/Data/Treyarch/Black Ops 7/DLC 1 - Astra Malorum/Easter Egg/makeAMEasterEggTools.swift
@@ -12,6 +12,39 @@ public func makeAMEasterEggTools() -> SerializableToolsRouter {
         makeAMLGM1AbhorrentCablesStreetlightsTool(),
         at: ["lgm-1 abhorrent cables streetlights"]
     )
+    
+    amEETools.router.register(
+        SerializableToolNode(
+            name: "bo7.am.easter.egg.planets.tool.name",
+            position: 2,
+            assetsImageName: "bo7.am.easter.egg.planets.tool.name.icon",
+            isSolver: true,
+            galleryRouter: nil
+        ),
+        at: ["planets"]
+    )
+
+    amEETools.router.register(
+        SerializableToolNode(
+            name: "bo7.am.easter.egg.thurston.reading.list.tool.name",
+            position: 3,
+            assetsImageName: "bo7.am.easter.egg.thurston.reading.list.icon",
+            isSolver: true,
+            galleryRouter: nil
+        ),
+        at: ["thurston reading list"]
+    )
+
+    amEETools.router.register(
+        SerializableToolNode(
+            name: "bo7.am.easter.egg.mars.pillars.puzzle.tool.name",
+            position: 3,
+            assetsImageName: "bo7.am.easter.egg.mars.pillars.puzzle.tool.name.icon",
+            isSolver: true,
+            galleryRouter: nil
+        ),
+        at: ["thurston reading list"]
+    )
 
     return amEETools
 }
