ea0db260ba92e35724ccd7fce57728534589bc21
= <niccolo.dellarocca@gmail.com>
2026-04-15 23:12:58 +0200

    Fixed mislinking in bo2 > buried

--- Sources/ZTronCarouselDataSource/Data/Treyarch/Black Ops 2/3 - Buried/Easter Egg/Warden Key/makeBuriedWardenKeyTool.swift
+++ Sources/ZTronCarouselDataSource/Data/Treyarch/Black Ops 2/3 - Buried/Easter Egg/Warden Key/makeBuriedWardenKeyTool.swift
@@ -2,9 +2,9 @@ import ZTronSerializable
 
 public func makeBuriedWardenKeyTool() -> SerializableToolNode {
     return SerializableToolNode(
-        name: "bo2.buried.easter.egg.shared.spheres.tool.name",
+        name: "bo2.buried.easter.egg.shared.warden.key.tool.name",
         position: 4,
-        assetsImageName: "bo2.buried.easter.egg.shared.spheres.icon",
-        galleryRouter: makeBuriedSpheres()
+        assetsImageName: "bo2.buried.easter.egg.shared.warden.key.icon",
+        galleryRouter: makeBuriedWadenKey()
     )
 }
